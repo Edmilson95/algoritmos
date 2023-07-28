5b7e482d043ffaabea48f935a08705f2595d6e84
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-07-28 15:53:15 -0300

    first commit

---
+++ src/main/java/org/example/GastoCombustivel.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class GastoCombustivel {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int horas = ler.nextInt();
+        int velocidade = ler.nextInt();
+
+        double distancia = horas * velocidade;
+
+        double gasolina = distancia / 12;
+
+        System.out.println(String.format("%.3f", gasolina));
+
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
