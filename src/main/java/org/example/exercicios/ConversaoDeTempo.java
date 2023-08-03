8a22bca70922667a435ea620112a545277d41030
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-08-03 16:52:48 -0300

    conversao de segundos para hora:min:seg

---
+++ src/main/java/org/example/exercicios/ConversaoDeTempo.java
@@ -0,0 +1,28 @@
+package org.example.exercicios;
+
+import java.sql.Time;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class ConversaoDeTempo {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int totalSegundos = ler.nextInt();
+
+        int qntHoras = totalSegundos / 3600;
+        totalSegundos = totalSegundos % 3600;
+
+        int qntMinutos = totalSegundos / 60;
+        totalSegundos = totalSegundos % 60;
+
+        int qntSegundos = totalSegundos;
+
+        System.out.println(qntHoras + ":" + qntMinutos + ":" + qntSegundos);
+
+        ler.close();
+    }
+
+
+
+}
