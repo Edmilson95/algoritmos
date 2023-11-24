066419d9ecaa9ed4e556bc88c3670dac0599a180
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-11-24 12:14:46 -0300

    algoritmo de ordenacao bubblesort

--- src/main/java/org/example/exercicios/iniciante/BubbleSort.java
+++ src/main/java/org/example/exercicios/iniciante/BubbleSort.java
@@ -1,2 +1,30 @@
-package org.example.exercicios.iniciante;public class BubbleSort {
+package org.example.exercicios.iniciante;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int vetor[] = {3,5,2,6,7,4,5};
+        int aux;
+        boolean controle;
+
+        for (int i = 0; i < vetor.length; i++){
+            controle = true;
+            for (int j = 0; j < (vetor.length - 1); j++){
+                if (vetor[j] > vetor[j + 1]){
+                    aux = vetor[j];
+                    vetor[j] = vetor[j+1];
+                    vetor[j + 1] = aux;
+                    controle = false;
+                }
+            }
+            if (controle){
+                break;
+            }
+        }
+
+        for (int i = 0; i < vetor.length; i++){
+            System.out.println(vetor[i] + " ");
+        }
+
+    }
 }
