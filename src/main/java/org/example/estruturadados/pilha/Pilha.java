461b8043597f90a60212682903a10d517732351c
Edmilson <edmilson.svic@gmail.com>
2024-06-21 21:45:31 -0300

    exercicios de pilhas

--- src/main/java/org/example/estruturadados/pilha/Pilha.java
+++ src/main/java/org/example/estruturadados/pilha/Pilha.java
@@ -13,4 +13,30 @@ public class Pilha<T> extends EstruturaEstatica<T> {
     public void empilha(T elemento){
         this.adiciona(elemento);
     }
+
+    public T topo(){
+        if (this.estaVazia()){
+            return null;
+        }
+
+        return this.elementos[tamanho-1];
+    }
+
+    public T desempilha(){
+
+        if (this.estaVazia()){
+            return null;
+        }
+
+//        T elemento = this.elementos[tamanho-1];
+//        tamanho--;
+//
+//        return elemento;
+
+        return this.elementos[--tamanho];
+    }
+
+    private boolean estaVazia() {
+        return tamanho == 0;
+    }
 }
