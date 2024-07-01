dcbcf24c7e1d70d23b998f6f26a80f5beba296bf
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2024-07-01 15:54:00 -0300

    exercicio de palindromo com pilhas

---
+++ src/main/java/org/example/estruturadados/pilha/labs/Exer05.java
@@ -0,0 +1,31 @@
+package org.example.estruturadados.pilha.labs;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Exer05 {
+    public static void main(String[] args) {
+        imprimeResultado("ABCD");
+        imprimeResultado("ADA");
+        imprimeResultado("ABCCBA");
+        imprimeResultado("MARIA");
+    }
+
+    public static void imprimeResultado(String palavra){
+        System.out.println(palavra + " é palindromo? " + testaPalindromo(palavra));
+    }
+    public static boolean testaPalindromo(String palavra){
+        Stack<Character> pilha = new Stack<>();
+        for (int i = 0; i < palavra.length(); i++){
+            pilha.push(palavra.charAt(i));
+        }
+        String palavraInversa = "";
+        while (!pilha.isEmpty()){
+            palavraInversa += pilha.pop();
+        }
+        if (palavraInversa.equalsIgnoreCase(palavra)){
+            return true;
+        }
+        return false;
+    }
+}
